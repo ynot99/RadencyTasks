d2432fc2a5f5f5c698504d201f15402929e99cfa
Vitalii Chornyi <vitalii.chornyi1999@gmail.com>
2023-04-08 18:21:53 +0400

    Modify back and add CORS for backend

--- HomeTask2/HomeTask2/Program.cs
+++ HomeTask2/HomeTask2/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.WithOrigins("http://localhost:4200", "https://radency-hometask3.onrender.com")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
--- HomeTask3/src/environments/environment.development.ts
+++ HomeTask3/src/environments/environment.development.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  apiBaseUrl: 'https://radency-fullstack.onrender.com',
-  // apiBaseUrl: 'https://localhost:5000',
+  apiBaseUrl: 'https://localhost:5000',
 };
